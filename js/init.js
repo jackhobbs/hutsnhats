8ba2dabee93aabd81535a0ae8bc81dd071d1c5e3
Zakaul Haque <zakihaqster@gmail.com>
2021-03-30 12:42:22 +0530

    demo
    Demo

---
+++ js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.slider').slider({
+      height:160
+    });
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
